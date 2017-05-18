db4ff4289729940a78535b33cb0f2eaed34e3685
Amit Srivastava <srivastava.amit@gmail.com>
2017-05-18 09:56:43 -0700

    emacs setup - packages

--- dot_emacs_dot_d/emacs_init.el
+++ dot_emacs_dot_d/emacs_init.el
@@ -1,5 +1,10 @@
 ;; Emacs config file
 
+;; Start
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 
 ;; Enable IDO mode and configuration
 (setq ido-enable-flex-matching t)
@@ -7,3 +12,6 @@
 (ido-mode 1)
 (setq ido-create-new-buffer 'always)
 (setq ido-file-extensions-order '(".org" ".txt" ".py" ".emacs" ".xml" ".el" ".ini" ".cfg" ".cnf"))
+
+;; put backups in one location
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
