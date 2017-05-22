1d7a8f7378be986a2dbbdea1c5621ef1a2f9e9b5
Amit Srivastava <srivastava.amit@gmail.com>
2017-05-21 21:30:58 -0700

    emacs more configuration - installing packages

--- dot_emacs_dot_d/emacs_init.el
+++ dot_emacs_dot_d/emacs_init.el
@@ -2,9 +2,29 @@
 
 ;; Start
 (require 'package)
-(package-initialize)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+; list the packages you want
+(setq package-list '(company powerline))
 
+; list the repositories containing them
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Basic Emacs setup
+(column-number-mode)
+(show-paren-mode)
 
 ;; Enable IDO mode and configuration
 (setq ido-enable-flex-matching t)
@@ -15,3 +35,12 @@
 
 ;; put backups in one location
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+
+;; Change "yes or no" to "y or n"
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Powerline
+(powerline-center-theme)
+(setq powerline-default-separator 'wave)
+
+
